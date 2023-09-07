95779b29ed03bda84d50fd1c16affea3d00455bb
Priyank038 <137056694+Priyank038@users.noreply.github.com>
2023-09-08 00:15:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# devfest-new-delhi-2023
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/devfest-new-delhi-2023-cfp-8o4z54)
---
+++ index.js
@@ -0,0 +1,31 @@
+// Import stylesheets
+import './style.css';
+import salt from './env/secret';
+
+function encryptString(str) {
+  let encryptedStr = '';
+  for (let i = 0; i <= str.length; i++) {
+    encryptedStr += String.fromCharCode(str.charCodeAt(i) + 2);
+  }
+  return encryptedStr;
+}
+
+function decryptString(str) {
+  let decryptedStr = '';
+  for (let i = 0; i < str.length; i++) {
+    decryptedStr += String.fromCharCode(
+      str.charCodeAt(i) - 2 // Subtract 2 to decrypt
+    );
+  }
+  return decryptedStr;
+}
+
+
+let encryptedUrl = `jvvru<11tcy0ikvjwdwugteqpvgpv0eqo1kcoitcycn1fgxhguv/fgnjk/42451ockp1TGCFOG0of`;
+let decryptedUrl = decryptString(encryptedUrl + salt);
+
+// Showing the output to the browser document as well
+let formLinkElement = document.getElementById('decryptedUrl');
+
+formLinkElement.href = decryptedUrl;
+formLinkElement.innerText = decryptedUrl;
